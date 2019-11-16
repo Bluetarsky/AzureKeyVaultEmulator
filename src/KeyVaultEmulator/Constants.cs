c2723f809d0f8119e6e11bf423fad924f7131847
Tim Blue <tmblue87@gmail.com>
2019-11-16 08:26:17 -0600

    Fix path

--- src/KeyVaultEmulator/Constants.cs
+++ src/KeyVaultEmulator/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string VOLUME_PATH = "/data";
+        public const string VOLUME_PATH = "data";
         public const string CERTIFICATES_FOLDER = "";
         public const string KEYS_FOLDER = "";
     }
